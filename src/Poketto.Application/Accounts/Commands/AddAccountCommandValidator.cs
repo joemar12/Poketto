4b8cd41d4d7b61121f50ef933e978bd3767d8d85
Joe-Mar Gonzales <rameoj18@gmail.com>
2023-03-02 22:47:42 +0800

    solution file structure reorg

---
+++ src/Poketto.Application/Accounts/Commands/AddAccountCommandValidator.cs
@@ -0,0 +1,33 @@
+﻿using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+using Poketto.Application.Common.Interfaces;
+
+namespace Poketto.Application.Accounts.Commands
+{
+    public class AddAccountCommandValidator : AbstractValidator<AddAccountCommand>
+    {
+        private readonly IApplicationDbContext _dbContext;
+        private readonly ICurrentUserService _currentUserService;
+        public AddAccountCommandValidator(IApplicationDbContext dbContext, ICurrentUserService currentUserService)
+        {
+            _dbContext = dbContext;
+            _currentUserService = currentUserService;
+
+            RuleFor(x => x.Name)
+                .NotEmpty().WithMessage("Account name is required.")
+                .MaximumLength(200).WithMessage("Account name must not exceed 200 characters.")
+                .MustAsync(BeUniqueName).WithMessage("Account name must be unique.");
+
+            RuleFor(x => x.Description)
+                .MaximumLength(500).WithMessage("Description must not exceed 500 characters.");
+        }
+
+        public async Task<bool> BeUniqueName(string name, CancellationToken cancellationToken)
+        {
+            var currentUser = _currentUserService.GetCurrentUser();
+            return await _dbContext.Accounts
+                .Where(x => x.OwnerUserId == currentUser)
+                .AllAsync(x => x.Name != name, cancellationToken);
+        }
+    }
+}
---
+++ src/Poketto.Application/Transactions/Commands/AddJournalEntryCommandValidator.cs
@@ -0,0 +1,33 @@
+﻿using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+using Poketto.Application.Common.Interfaces;
+
+namespace Poketto.Application.Transactions.Commands
+{
+    public class AddJournalEntryCommandValidator : AbstractValidator<AddJournalEntryCommand>
+    {
+        private readonly IApplicationDbContext _dbContext;
+        private readonly ICurrentUserService _currentUserService;
+        public AddJournalEntryCommandValidator(IApplicationDbContext dbContext, ICurrentUserService currentUserService)
+        {
+            _dbContext = dbContext;
+            _currentUserService = currentUserService;
+
+            RuleFor(x => x.RefCode)
+                .NotEmpty().WithMessage("RefCode must not be empty.")
+                .MaximumLength(100).WithMessage("RefCode must not exceed 100 characters.")
+                .MustAsync(BeUniqueRefCode).WithMessage("RefCode must be unique.");
+
+            RuleFor(x => x.Description)
+                .MaximumLength(500).WithMessage("Description must not exceed 500 characters.");
+        }
+
+        public async Task<bool> BeUniqueRefCode(string refCode, CancellationToken cancellationToken)
+        {
+            var currentUser = _currentUserService.GetCurrentUser();
+            return await _dbContext.JournalEntries
+                .Where(x => x.OwnerUserId == currentUser)
+                .AllAsync(x => x.RefCode != refCode);
+        }
+    }
+}
